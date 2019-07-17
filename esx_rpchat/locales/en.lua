b1a8ce6b8d816010a2fc4bddcb67ddbb80378697
NotHyperDavis <51684699+NotHyperDavis@users.noreply.github.com>
2019-07-17 16:27:58 +0100

    esx_rpchat
    the original rpchat but with /anon and /alt

---
+++ esx_rpchat/locales/en.lua
@@ -0,0 +1,14 @@
+Locales['en'] = {
+  ['ooc_prefix'] = 'OOC | %s',
+  ['twt_help'] = 'send a tweet',
+  ['twt_prefix'] = '^0[^4Twitter^0] (^5@%s^0)',
+  ['me_help'] = 'personal action',
+  ['me_prefix'] = 'me | %s',
+  ['do_help'] = 'RP information',
+  ['do_prefix'] = 'do | %s',
+  ['anon_prefix'] = '^1[^1Anonimo^1] | %s',
+  ['alt_prefix'] = '^3[^3Altifalante^3] | %s',
+  ['generic_argument_name'] = 'message',
+  ['generic_argument_help'] = 'the message',
+  ['unknown_command'] = '^3%s^0 is not a valid command!',
+}
